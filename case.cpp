d55bc8067e614b3162a3f123ed9213f4b23417f7
7k7k12345 <1655908544@qq.com>
2024-08-09 23:23:41 +0800

    修复了几个bug

--- case.cpp
+++ case.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main()
+int main(){
 
    int type;
    scanf("%d",&type);
@@ -10,7 +10,7 @@ break;
 case 2:
 printf("早上好");
 break;
-rase 3:
+case 3:
 printf("晚上好");
 break;
 default:
@@ -18,3 +18,4 @@ printf("你在说啥?");
 break;
 }
 return 0;
+}
